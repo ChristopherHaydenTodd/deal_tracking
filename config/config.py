9c0fe46af358f173ab6e862897c782069e234646
Christopher Todd <Christopher_Todd@cable.comcast.com>
2016-05-11 23:32:09 -0400

    adding Python config file. Adding Log configs and Hip2Save RSS Feed URL

---
+++ config/config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+    Main Config File for Hip2Save Project
+"""
+
+import logging
+
+class Config(object):
+
+    ###
+    # Log Configs
+    ###
+
+    LOG_LEVEL = logging.INFO
+    LOG_FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
+    LOG_DATEFORMAT = '%a, %d %b %Y %H:%M:%S'
+    LOG_FILEMODE = 'a'
+
+    ###
+    # Connection Configs
+    ###
+
+    HIP_2_SAVE_RSS_URL = 'https://hip2save.com/feed/'
+
+
